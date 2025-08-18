a37191886d2a33e6339291fd1fc6b7e015bc0c1a
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-08-18 23:23:28 +0100

    test: nginx and certbot added

--- manage_restaurant_api/manage_restaurant_api/settings.py
+++ manage_restaurant_api/manage_restaurant_api/settings.py
@@ -144,6 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_ROOT = "/app/media"
+MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
