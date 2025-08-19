675f1a872a6a307b53b19d4dbdae957f099d92a9
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-08-19 15:02:34 +0100

    test

--- manage_restaurant_api/entrypoint.sh
+++ manage_restaurant_api/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/ash
 
 echo "Apply database migrations"
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+python manage.py makemigrations 
+python manage.py migrate 
+#python manage.py collectstatic --noinput
 
 exec "$@"
